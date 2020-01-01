b0ada9170c9de6e225e58620c3475017638f2576
toehead2001 <toehead2001@users.noreply.github.com>
2020-01-01 14:09:22 -0700

    Add sample code for Shapes and FileTypes

---
+++ NewShape.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PaintDotNet.Effects
+{
+    partial class NewShape
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgGadioButton = new System.Windows.Forms.RadioButton();
+            this.ggRadioButton = new System.Windows.Forms.RadioButton();
+            this.sgRadioButton = new System.Windows.Forms.RadioButton();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pgGadioButton
+            // 
+            this.pgGadioButton.AutoSize = true;
+            this.pgGadioButton.Location = new System.Drawing.Point(12, 37);
+            this.pgGadioButton.Name = "pgGadioButton";
+            this.pgGadioButton.Size = new System.Drawing.Size(235, 19);
+            this.pgGadioButton.TabIndex = 5;
+            this.pgGadioButton.TabStop = true;
+            this.pgGadioButton.Text = "SimpleGeometryShape - PathGeometry ";
+            this.pgGadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ggRadioButton
+            // 
+            this.ggRadioButton.AutoSize = true;
+            this.ggRadioButton.Location = new System.Drawing.Point(12, 62);
+            this.ggRadioButton.Name = "ggRadioButton";
+            this.ggRadioButton.Size = new System.Drawing.Size(241, 19);
+            this.ggRadioButton.TabIndex = 4;
+            this.ggRadioButton.TabStop = true;
+            this.ggRadioButton.Text = "SimpleGeometryShape - GeometryGroup";
+            this.ggRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // sgRadioButton
+            // 
+            this.sgRadioButton.AutoSize = true;
+            this.sgRadioButton.Location = new System.Drawing.Point(12, 12);
+            this.sgRadioButton.Name = "sgRadioButton";
+            this.sgRadioButton.Size = new System.Drawing.Size(245, 19);
+            this.sgRadioButton.TabIndex = 3;
+            this.sgRadioButton.TabStop = true;
+            this.sgRadioButton.Text = "SimpleGeometryShape - StreamGeometry";
+            this.sgRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.okButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.Location = new System.Drawing.Point(132, 114);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 24);
+            this.okButton.TabIndex = 6;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cancelButton.Location = new System.Drawing.Point(213, 114);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 24);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // NewShape
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.ClientSize = new System.Drawing.Size(300, 150);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.pgGadioButton);
+            this.Controls.Add(this.ggRadioButton);
+            this.Controls.Add(this.sgRadioButton);
+            this.IconName = "Shape";
+            this.Name = "NewShape";
+            this.Text = "New Shape";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton pgGadioButton;
+        private System.Windows.Forms.RadioButton ggRadioButton;
+        private System.Windows.Forms.RadioButton sgRadioButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ NewShape.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PaintDotNet.Effects
+{
+    internal partial class NewShape : ChildFormBase
+    {
+        internal string ShapeCode;
+
+        internal NewShape()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            if (this.ggRadioButton.Checked)
+            {
+                this.ShapeCode = geometryGroup;
+            }
+            else if (this.pgGadioButton.Checked)
+            {
+                this.ShapeCode = pathGeometry;
+            }
+            else
+            {
+                this.ShapeCode = streamGeometry;
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private const string streamGeometry = "" +
+            "<ps:SimpleGeometryShape\r\n" +
+            "    xmlns=\"clr-namespace:PaintDotNet.UI.Media;assembly=PaintDotNet.Framework\"\r\n" +
+            "    xmlns:ps=\"clr-namespace:PaintDotNet.Shapes;assembly=PaintDotNet.Framework\"\r\n" +
+            "    DisplayName=\"Square\"\r\n" +
+            "    Geometry=\"F0 M 0,0 L 100,0 L 100,100 L 0,100 Z\" />\r\n";
+
+        private const string pathGeometry = "" +
+            "<ps:SimpleGeometryShape\r\n" +
+            "    xmlns=\"clr-namespace:PaintDotNet.UI.Media;assembly=PaintDotNet.Framework\"\r\n" +
+            "    xmlns:ps=\"clr-namespace:PaintDotNet.Shapes;assembly=PaintDotNet.Framework\"\r\n" +
+            "    DisplayName=\"Cube\">\r\n" +
+            "    <PathGeometry FillRule = \"Nonzero\" >\r\n" +
+            "        <PathFigure IsClosed=\"True\" IsFilled=\"True\" StartPoint=\"0,22\">\r\n" +
+            "            <LineSegment Point = \"58,0\" IsSmoothJoin=\"True\" />\r\n" +
+            "            <LineSegment Point = \"116,22\" IsSmoothJoin=\"True\" />\r\n" +
+            "            <LineSegment Point = \"116,93\" IsSmoothJoin=\"True\" />\r\n" +
+            "            <LineSegment Point = \"58,116\" IsSmoothJoin=\"True\" />\r\n" +
+            "            <LineSegment Point = \"0,93\" IsSmoothJoin=\"True\" />\r\n" +
+            "            <LineSegment Point = \"0,22\" IsSmoothJoin=\"True\" />\r\n" +
+            "        </PathFigure>\r\n" +
+            "        <PathFigure IsClosed = \"False\" IsFilled=\"False\" StartPoint=\"58,46\">\r\n" +
+            "            <LineSegment Point = \"0,22\" />\r\n" +
+            "        </PathFigure >\r\n" +
+            "        <PathFigure IsClosed=\"False\" IsFilled=\"False\" StartPoint=\"58,46\">\r\n" +
+            "            <LineSegment Point = \"58,116\" />\r\n" +
+            "        </PathFigure >\r\n" +
+            "        <PathFigure IsClosed=\"False\" IsFilled=\"False\" StartPoint=\"58,46\">\r\n" +
+            "            <LineSegment Point = \"116,22\" />\r\n" +
+            "        </PathFigure >\r\n" +
+            "    </PathGeometry >\r\n" +
+            "</ps:SimpleGeometryShape>\r\n";
+
+        private const string geometryGroup = "" +
+            "<ps:SimpleGeometryShape\r\n" +
+            "    xmlns=\"clr-namespace:PaintDotNet.UI.Media;assembly=PaintDotNet.Framework\"\r\n" +
+            "    xmlns:ps=\"clr-namespace:PaintDotNet.Shapes;assembly=PaintDotNet.Framework\"\r\n" +
+            "    DisplayName=\"Rounded Rectangle\">\r\n" +
+            "        <GeometryGroup>\r\n" +
+            "            <RectangleGeometry Rect=\"0,0,100,160\" RadiusX=\"5\" RadiusY=\"5\" />\r\n" +
+            "            <RectangleGeometry Rect=\"2,2,96,156\" RadiusX=\"5\" RadiusY=\"5\" />\r\n" +
+            "        </GeometryGroup>\r\n" +
+            "</ps:SimpleGeometryShape>\r\n";
+    }
+}
