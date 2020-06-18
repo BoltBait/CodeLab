277cd58d6bb99555c426c01cd10f52a2d6d03103
toehead2001 <toehead2001@users.noreply.github.com>
2020-06-18 03:24:27 -0600

    Fix for when there is whitespace before/after dots
    Example:
    
    Rectangle selection = EnvironmentParameters
        .SelectionBounds;
    
    That whitespace would break the Intelligent Assistance features.

--- CodeTextBox.cs
+++ CodeTextBox.cs
@@ -937,7 +937,11 @@ namespace PaintDotNet.Effects
             {
                 char c = strippedText[posIndex - 1];
 
-                if (!(char.IsLetterOrDigit(c) || c == '_' || c == '.'))
+                if (char.IsWhiteSpace(c))
+                {
+                    // no-op
+                }
+                else if (!(char.IsLetterOrDigit(c) || c == '_' || c == '.'))
                 {
                     return strippedText.Substring(posIndex);
                 }
@@ -960,7 +964,11 @@ namespace PaintDotNet.Effects
             {
                 char c = this.GetCharAt(tokenPos - 1);
 
-                if (c == ')' || c == '>')
+                if (char.IsWhiteSpace(c))
+                {
+                    // no-op
+                }
+                else if (c == ')' || c == '>')
                 {
                     tokenPos = this.BraceMatch(tokenPos - 1) + 1;
                 }
@@ -2086,7 +2094,7 @@ namespace PaintDotNet.Effects
         private MemberInfo GetMember(int position, out int length)
         {
             length = 0;
-            string lastWords = GetLastWords(position);
+            string lastWords = GetLastWords(position).StripWhitespace();
             string[] tokens = lastWords.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
             if (tokens.Length == 0)
             {
