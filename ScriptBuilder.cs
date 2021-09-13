9bd952c4d4d822dd50c1b5f1b629fe7fe7630bd9
toehead2001 <toehead2001@users.noreply.github.com>
2021-09-12 20:55:42 -0700

    Switch to C# 9.0

--- ScriptBuilder.cs
+++ ScriptBuilder.cs
@@ -37,7 +37,8 @@ namespace PaintDotNet.Effects
         private static readonly Regex preRenderRegex = new Regex(@"void\s+PreRender\s*\(\s*Surface\s+dst\s*,\s*Surface\s+src\s*\)\s*{(.|\s)*}", RegexOptions.Singleline);
 
         private static readonly IEnumerable<MetadataReference> references = Intelli.ReferenceAssemblies.Select(a => MetadataReference.CreateFromFile(a.Location));
-        private static readonly CSharpParseOptions parseOptions = CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp7_3);
+        private static readonly CSharpParseOptions parseOptions = CSharpParseOptions.Default
+            .WithLanguageVersion(LanguageVersion.CSharp9); // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/configure-language-version
         private static CSharpCompilationOptions compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true, optimizationLevel: OptimizationLevel.Release);
         private static IEnumerable<string> warningsToIgnore = Array.Empty<string>();
 
