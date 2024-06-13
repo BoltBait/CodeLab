b1281683795b2fb154727e3195adc04fcd4d27fd
toehead2001 <toehead2001@users.noreply.github.com>
2024-06-12 19:15:24 -0700

    Remove temporary workaround that was need for FastDebug

--- FastDebug/Program.cs
+++ FastDebug/Program.cs
@@ -1,9 +1,6 @@
 ﻿#if FASTDEBUG
 using System;
 using System.Windows.Forms;
-using TerraFX.Interop.Windows;
-using TerraFX.Interop.WinRT;
-using Windows.System;
 
 namespace PdnCodeLab
 {
@@ -15,29 +12,10 @@ namespace PdnCodeLab
         [STAThread]
         private static void Main()
         {
-            DispatcherQueueController dispatcherQueueController = CreateForCurrentThread();
-
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CodeLabConfigDialog(RenderPreset.Regular));
-
-            dispatcherQueueController.ShutdownQueueAsync();
-        }
-
-        private static unsafe DispatcherQueueController CreateForCurrentThread()
-        {
-            using ComPtr<IDispatcherQueueController> spController = default;
-            HRESULT hr = TerraFX.Interop.Windows.Windows.CreateDispatcherQueueController(
-                new DispatcherQueueOptions()
-                {
-                    dwSize = (uint)sizeof(DispatcherQueueOptions),
-                    apartmentType = DISPATCHERQUEUE_THREAD_APARTMENTTYPE.DQTAT_COM_STA,
-                    threadType = DISPATCHERQUEUE_THREAD_TYPE.DQTYPE_THREAD_CURRENT
-                },
-                spController.GetAddressOf());
-
-            return DispatcherQueueController.FromAbi((IntPtr)spController.Get());
         }
     }
 }
