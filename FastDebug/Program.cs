2317e324a6395d59804a3c483fb4a05dc36cae7e
toehead2001 <toehead2001@users.noreply.github.com>
2025-09-15 23:21:53 -0700

    get Direct2DControls working in FastDebug

--- FastDebug/Program.cs
+++ FastDebug/Program.cs
@@ -1,5 +1,8 @@
 ﻿#if FASTDEBUG
 using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace PdnCodeLab
@@ -15,6 +18,23 @@ namespace PdnCodeLab
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // hack to get Controls derived from Direct2DControl working in FastDebug
+            {
+                Control syncContextControl = new Control(); // this forces WinForms SyncContext to be installed
+
+                MethodInfo syncContextInstall = AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .First(a => a.GetName().Name == "PaintDotNet.Base")
+                    .GetTypes()
+                    .First(a => a.Name == "PdnSynchronizationContext")
+                    .GetMethod("Install", BindingFlags.NonPublic | BindingFlags.Static);
+
+                Type delegateType = Expression.GetDelegateType(syncContextInstall.ReturnType);
+
+                syncContextInstall.CreateDelegate(delegateType).DynamicInvoke();
+            }
+
             Application.Run(new CodeLabConfigDialog(RenderPreset.Regular));
         }
     }
